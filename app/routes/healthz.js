fb70dcc48d1ae8ce521c063f5d0264586ad42478
John Watson <john.watson1@defra.gov.uk>
2023-08-07 11:04:50 +0100

    Add routes

--- app/routes/healthz.js
+++ app/routes/healthz.js
@@ -1,5 +1,7 @@
+const { GET } = require('../constants/http-verbs')
+
 module.exports = {
-  method: 'GET',
+  method: GET,
   path: '/healthz',
   handler: (request, h) => {
     return h.response('ok').code(200)
---
+++ app/routes/sign-in.js
@@ -0,0 +1,36 @@
+const Joi = require('joi')
+const { GET, POST } = require('../../constants/verbs')
+
+module.exports = [{
+  method: GET,
+  path: '/sign-in',
+  handler: (_request, h) => {
+    return h.view('sign-in')
+  }
+},
+{
+  method: POST,
+  path: '/sign-in',
+  options: {
+    validate: {
+      payload: Joi.object({
+        crn: Joi.number().integer().required(),
+        password: Joi.string().required()
+      }),
+      failAction: async (_request, h, _error) => {
+        return h.view('sign-in', {
+          message: 'Your CRN and/or password is incorrect'
+        }).takeover()
+      }
+    },
+    handler: async (request, h) => {
+      try {
+        return h.redirect('/home')
+      } catch {
+        return h.view('sign-in', {
+          message: 'Your CRN and/or password is incorrect'
+        })
+      }
+    }
+  }
+}]
