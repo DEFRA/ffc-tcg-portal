2f29384f1fca2206a772b3238be70889f9016ad2
John Watson <john.watson1@defra.gov.uk>
2023-08-07 13:08:28 +0100

    Add plugins

--- app/server.js
+++ app/server.js
@@ -8,6 +8,8 @@ const createServer = async () => {
 
   await server.register(require('@hapi/inert'))
   await server.register(require('./plugins/router'))
+  await server.register(require('./plugins/crumb'))
+  await server.register(require('./plugins/errors'))
   await server.register(require('./plugins/views'))
   await server.register(require('./plugins/logging'))
 
