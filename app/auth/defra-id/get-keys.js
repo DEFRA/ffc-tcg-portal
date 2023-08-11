ad81db8157b51461c1206eeacb94f05295c559c4
John Watson <john.watson1@defra.gov.uk>
2023-08-11 14:07:29 +0100

    Get Defra ID keys

--- app/auth/defra-id/get-keys.js
+++ app/auth/defra-id/get-keys.js
@@ -1,5 +1,16 @@
+const Wreck = require('@hapi/wreck')
+const jwkToPem = require('jwk-to-pem')
+const { authConfig } = require('../../config')
+
 const getKeys = async () => {
-  return 'secret'
+  const url = `https//:${authConfig.tenant}.b2clogin.com/${authConfig.tenant}.onmicrosoft.com/${authConfig.policy}/discovery/v2.0/keys`
+
+  const { payload } = await Wreck.get(url, {
+    json: true
+  })
+
+  const { keys } = payload
+  return jwkToPem(keys[0])
 }
 
 module.exports = {
