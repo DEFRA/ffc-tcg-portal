e6d85ae74fa73ca2525e4be2f7c40a50f254fc75
Simon Dunn <simondunn90@gmail.com>
2023-08-16 15:26:34 +0100

    update auth url query

--- app/auth/defra-id/get-authorization-url.js
+++ app/auth/defra-id/get-authorization-url.js
@@ -1,15 +1,17 @@
 const { authConfig } = require('../../config')
 
 const getAuthorizationUrl = () => {
-  const host = `https//:${authConfig.tenant}.b2clogin.com`
-  const path = `/${authConfig.tenant}.onmicrosoft.com/${authConfig.policy}/oauth2/v2.0/authorize`
+  const host = `https://${authConfig.tenant}.b2clogin.com`
+  const path = `/${authConfig.tenant}.onmicrosoft.com/oauth2/v2.0/authorize`
   const query = [
+    `p=${authConfig.policy}`,
     `client_id=${authConfig.clientId}`,
     `service_id=${authConfig.serviceId}`,
     'nonce=defaultNonce',
     `redirect_uri=${authConfig.redirectUrl}`,
     `scope=openid offline_access ${authConfig.clientId}`,
     'response_type=code',
+    'prompt=login',
     'response_mode=form_post'
   ].join('&')
   return `${host}${path}?${query}`
