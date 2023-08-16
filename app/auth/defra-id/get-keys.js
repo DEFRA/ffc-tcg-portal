fa88ed40c18d7f44841eb32283e4f39ce5216f44
Simon Dunn <simondunn90@gmail.com>
2023-08-16 11:51:48 +0100

    fix typo in url

--- app/auth/defra-id/get-keys.js
+++ app/auth/defra-id/get-keys.js
@@ -3,8 +3,7 @@ const jwkToPem = require('jwk-to-pem')
 const { authConfig } = require('../../config')
 
 const getKeys = async () => {
-  const url = `https//:${authConfig.tenant}.b2clogin.com/${authConfig.tenant}.onmicrosoft.com/${authConfig.policy}/discovery/v2.0/keys`
-
+  const url = `https://${authConfig.tenant}.b2clogin.com/${authConfig.tenant}.onmicrosoft.com/${authConfig.policy}/discovery/v2.0/keys`
   const { payload } = await Wreck.get(url, {
     json: true
   })
