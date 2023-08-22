12a44d57d0b4d4d4f92bb340fcee62d8d42615b0
Simon Dunn <simondunn90@gmail.com>
2023-08-22 15:05:13 +0100

    display all users

--- app/routes/people.js
+++ app/routes/people.js
@@ -9,11 +9,13 @@ module.exports = [{
   options: { auth: { strategy: 'jwt', scope: [USER] } },
   handler: async (request, h) => {
     try {
-      const people = await Wreck.get('http://ffc-tcg-abaco-agri-stub:3052/master/api-priv/v1/parties', {
+      const promise = Wreck.request(GET, 'http://ffc-tcg-abaco-agri-stub:3052/master/api-priv/v1/parties', {
         headers: {
           authorization: `Bearer ${request.state.tcg_auth_token}`
         }
       })
+      const res = await promise
+      const people = await Wreck.read(res, { json: true })
       return h.view('people', { people })
     } catch (err) {
       throw new Error()
