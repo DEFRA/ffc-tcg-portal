ca90455923f62e42ca413e9a2dcbca5aa6167701
Simon Dunn <simondunn90@gmail.com>
2023-08-22 13:14:31 +0100

    GET from Abaco stub

--- app/routes/people.js
+++ app/routes/people.js
@@ -1,22 +1,22 @@
 const { GET } = require('../constants/http-verbs')
+// const { AUTH_COOKIE_NAME } = require('../constants/cookies')
 const { USER } = require('../auth/scopes')
-
-const people = [
-  {
-    id: 1,
-    name: 'Simon Dunn'
-  },
-  {
-    id: 2,
-    name: 'Luke Skywalker'
-  }
-]
+const Wreck = require('@hapi/wreck')
 
 module.exports = [{
   method: GET,
   path: '/people',
   options: { auth: { strategy: 'jwt', scope: [USER] } },
-  handler: (_request, h) => {
-    return h.view('people', { people })
+  handler: async (request, h) => {
+    try {
+      const people = await Wreck.get('http://ffc-tcg-abaco-agri-stub:3052/master/api-priv/v1/parties', {
+        headers: {
+          authorization: `Bearer ${request.state.tcg_auth_token}`
+        }
+      })
+      return h.view('people', { people })
+    } catch (err) {
+      throw new Error()
+    }
   }
 }]
