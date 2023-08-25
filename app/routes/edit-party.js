828dc3e05d1b1fd6e88deac15753ba064760c567
Simon Dunn <simondunn90@gmail.com>
2023-08-25 15:13:39 +0100

    edit party

---
+++ app/routes/edit-party.js
@@ -0,0 +1,53 @@
+const Joi = require('joi')
+const Wreck = require('@hapi/wreck')
+const { GET, POST } = require('../constants/http-verbs')
+const { USER } = require('../auth/scopes')
+
+module.exports = [{
+  method: GET,
+  path: '/people/edit-party',
+  options: { auth: { strategy: 'jwt', scope: [USER] } },
+  handler: (request, h) => {
+    const user = {
+      name: request.query.user,
+      id: request.query.id
+    }
+    return h.view('edit-party', { user })
+  }
+},
+{
+  method: POST,
+  path: '/people/edit-party',
+  options: {
+    validate: {
+      payload: Joi.object({
+        firstname: Joi.string().required(),
+        lastname: Joi.string().required(),
+        userid: Joi.string().required()
+      }),
+      failAction: async (request, h, _error) => {
+        return h.view('edit-party', {
+          message: 'First name and last name are required'
+        }).takeover()
+      }
+    }
+  },
+  handler: async (request, h) => {
+    try {
+      await Wreck.put(`http://ffc-tcg-abaco-agri-stub:3052/master/api-priv/v1/parties/${request.payload.userid}`,
+        {
+          headers: {
+            authorization: `Bearer ${request.state.tcg_auth_token}`
+          },
+          payload: {
+            firstname: request.payload.firstname,
+            lastname: request.payload.lastname
+          }
+        })
+      return h.redirect('/people')
+    } catch (err) {
+      throw new Error()
+    }
+  }
+}
+]
