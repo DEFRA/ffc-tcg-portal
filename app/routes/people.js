4f32dde94c9e9b9873e1bba29af7da4d591f8e5b
John Watson <john.watson1@defra.gov.uk>
2023-09-06 12:15:58 +0100

    Update response

--- app/routes/people.js
+++ app/routes/people.js
@@ -8,14 +8,14 @@ module.exports = [{
   path: '/people',
   options: { auth: { strategy: 'jwt', scope: [USER] } },
   handler: async (request, h) => {
-    const { response, payload: people } = await Wreck.get(`${serverConfig.abacoEndpoint}/party-registry/master/api-priv/v1/parties?lastName=%`, {
+    const { res, payload: people } = await Wreck.get(`${serverConfig.abacoEndpoint}/party-registry/master/api-priv/v1/parties?lastName=%`, {
       headers: {
         authorization: `Bearer ${request.state.tcg_auth_token}`
       },
       agent: Wreck.agents.httpsAllowUnauthorized,
       json: true
     })
-    console.log(response)
+    console.log(res)
     return h.view('people', { people: people.records })
   }
 }]
