405bf0063a7b1575f99337d009eb9c86f834ac95
Simon Dunn <simondunn90@gmail.com>
2023-09-21 11:11:09 +0100

    add encoding to GET (#21)

--- app/routes/people.js
+++ app/routes/people.js
@@ -8,7 +8,7 @@ module.exports = [{
   path: '/people',
   options: { auth: { strategy: 'jwt', scope: [USER] } },
   handler: async (request, h) => {
-    const { payload: people } = await Wreck.get(`${serverConfig.abacoEndpoint}/party-registry/master/api-priv/v1/parties?lastName=%`, {
+    const { payload: people } = await Wreck.get(`${serverConfig.abacoEndpoint}/party-registry/master/api-priv/v1/parties?lastName=%25`, {
       headers: {
         authorization: `Bearer ${request.state.tcg_auth_token}`
       },
