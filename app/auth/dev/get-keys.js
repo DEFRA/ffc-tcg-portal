b74d4850bbd53e84f435be582943ad5a9fee475a
John Watson <42574977+johnwatson484@users.noreply.github.com>
2023-11-01 13:00:19 +0000

    Update dev keys (#22)

--- app/auth/dev/get-keys.js
+++ app/auth/dev/get-keys.js
@@ -1,11 +1,17 @@
 const util = require('util')
 const { generateKeyPair } = require('crypto')
 const generateKeyPairPromise = util.promisify(generateKeyPair)
+const { authConfig } = require('../../config')
 
 let privateKey
 let publicKey
 
 const getKeys = async () => {
+  if (authConfig.devAuthPrivateKey && authConfig.devAuthPublicKey) {
+    privateKey = authConfig.devAuthPrivateKey
+    publicKey = authConfig.devAuthPublicKey
+  }
+
   if (!privateKey || !publicKey) {
     const { privateKey: newPrivateKey, publicKey: newPublicKey } = await generateKeyPairPromise('rsa', {
       modulusLength: 4096,
